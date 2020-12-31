7a8dabb54a70824c139d58465d7e0a613b2afa45
Saurabh Mishra <saurab.mish@gmail.com>
2020-12-31 15:04:13 -0800

    problem 1

--- multiples-of-3-and-5/multiples_of_3_and_5.go
+++ multiples-of-3-and-5/multiples_of_3_and_5.go
@@ -3,16 +3,15 @@ package main
 import "fmt"
 
 func Multiples() int {
-    sum := 0
-    for i := 0; i < 1000; i++ {
-        if i % 3 == 0 || i % 5 == 0 {
-            sum += i
-        }
-    }
-    return sum
+	sum := 0
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum
 }
 
 func main() {
-    fmt.Println(Multiples())
+	fmt.Println(Multiples())
 }
-
--- multiples-of-3-and-5/multiples_of_3_and_5_test.go
+++ multiples-of-3-and-5/multiples_of_3_and_5_test.go
@@ -3,11 +3,10 @@ package main
 import "testing"
 
 func TestMultiples(t *testing.T) {
-    got := Multiples()
-    want := 233168
+	got := Multiples()
+	want := 233168
 
-    if got != want {
-        t.Errorf("Got: %q Want: %q", got, want)
-    }
+	if got != want {
+		t.Errorf("Got: %q Want: %q", got, want)
+	}
 }
-
