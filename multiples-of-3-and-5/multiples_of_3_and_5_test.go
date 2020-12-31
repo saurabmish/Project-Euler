d3751c5c1c919ae650152c656dd6bc34101a7c91
Saurabh Mishra <saurab.mish@gmail.com>
2020-12-31 10:34:40 -0800

    source code and test for problem 1: multiples of 3 and 5 less than 1000

---
+++ multiples-of-3-and-5/multiples_of_3_and_5_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestMultiples(t *testing.T) {
+    got := Multiples()
+    want := 233168
+
+    if got != want {
+        t.Errorf("Got: %q Want: %q", got, want)
+    }
+}
+
