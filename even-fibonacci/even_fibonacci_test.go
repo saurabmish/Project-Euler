6423cbbae3be53ff603b374ae09c7a51c1ab5d34
Saurabh Mishra <saurab.mish@gmail.com>
2021-01-07 11:51:44 -0800

    comprehensive test cases

--- even-fibonacci/even_fibonacci_test.go
+++ even-fibonacci/even_fibonacci_test.go
@@ -4,8 +4,7 @@ import "testing"
 
 func TestEvenFibonacci(t *testing.T) {
 
-	/*
-	t.Run("Cutoff greater than sum of first two even numbers", func(t *testing.T) {
+	t.Run("Cutoff greater than the sum of first two even numbers", func(t *testing.T) {
 		got := EvenFibonacci(4e6)
 		want := 4613732
 
@@ -13,11 +12,24 @@ func TestEvenFibonacci(t *testing.T) {
 			t.Errorf("Error in computation; Got: %d Want: %d", got, want)
 		}
 	})
-	*/
-	got := EvenFibonacci(4e6)
-	want := 4613732
 
-	if got != want {
-		t.Errorf("Error in computation; Got: %d Want: %d", got, want)
-	}
+	// failing test 1
+	t.Run("Cutoff less than the sum of first two even numbers", func(t *testing.T) {
+		got := EvenFibonacci(4)
+		want := 10
+
+		if got != want {
+			t.Errorf("Cutoff parameter should be more than 10")
+		}
+	})
+
+	// failing test 2
+	t.Run("Cutoff equal to the sum of first two even numbers", func(t *testing.T) {
+		got := EvenFibonacci(10)
+		want := 10
+
+		if got != want {
+			t.Errorf("Cutoff parameter should be more than 10")
+		}
+	})
 }
